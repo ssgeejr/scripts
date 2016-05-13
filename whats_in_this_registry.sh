e73dab3aae6b4c5634e3a3708756f59fd206cdbf
Steve Gee <ioexcept@gmail.com>
2016-05-13 11:24:29 -0500

    Update whats_in_this_registry.sh

--- whats_in_this_registry.sh
+++ whats_in_this_registry.sh
@@ -1,19 +1,57 @@
-#!/bin/bash
-DOCKER_REG_PUBLISH_PORT=5000
-DOCKER_REG_HOST=localhost
-check_docker_registry() {
-  echo "Contents of the local Docker registry:"
-  IMAGE_LIST="collection cubrid dbm-bulk-loader dbm-ilm dbm-jdbc-service diamond-collector"
-  IMAGE_LIST="$IMAGE_LIST elk grafana haproxy influxdb iris-properties-webservice model-manager provisioning"
-  IMAGE_LIST="$IMAGE_LIST logstash-shipper rabbitmq rancher/server rancher/agent redis sensu-api sensu-client sensu-server sensu-uchiwa"
-  IMAGE_LIST="$IMAGE_LIST ui-dbm-query ui-discovery"
-  IMAGE_LIST="$IMAGE_LIST ansible pypi-repo yum-repo"
-  # This prints the contents of the Registy
-  curl http://$DOCKER_REG_HOST:$DOCKER_REG_PUBLISH_PORT/v2/_catalog
-  echo "###########################################"
-  for IMAGE in $IMAGE_LIST
-  do
-    curl http://$DOCKER_REG_HOST:$DOCKER_REG_PUBLISH_PORT/v2/$IMAGE/tags/list
-  done
-}
-check_docker_registry
+#!/usr/bin/env python
+import urllib2, argparse, json
+defaultRegistryPort="80"
+
+## Parse command line options
+parser = argparse.ArgumentParser()
+parser.add_argument("registry", help="Docker Registry host")
+parser.add_argument("--port", "-p", help="Docker Registry port, default "+defaultRegistryPort,
+                    default=defaultRegistryPort)
+args = parser.parse_args()
+
+class registry(object):
+
+    def __init__(self,server,port=80):
+        self.server=server
+        self.port=port
+
+    def baseUrl(self):
+        return "http://"+self.server+":"+self.port+"/v2/"
+
+    def catalogUrl(self):
+        return self.baseUrl()+"_catalog"
+
+    def tagUrl(self,image):
+        return self.baseUrl()+image+"/tags/list"
+
+    def imageList(self):
+        images=[]
+        catJson=urllib2.urlopen(self.catalogUrl())
+        catDict=json.loads(catJson.read())
+        for image in catDict['repositories']:
+            images.append(image)
+        return images
+
+    def tagList(self, image):
+        tags=[]
+        tagJson=urllib2.urlopen(self.tagUrl(image))
+        tagDict=json.loads(tagJson.read())
+        for tag in tagDict['tags']:
+            tags.append(tag)
+        return tags
+
+    def manifest(self, image, tag='latest'):
+        manifestUrl=self.baseUrl()+image+"/manifests/"+tag
+        manJson=urllib2.urlopen(manifestUrl)
+        manDict=json.loads(manJson.read())
+        return manDict
+
+    def labels(self, image, tag='latest'):
+        labels=[]
+        manifest=self.manifest(image=image,tag=tag)
+        return
+
+reg=registry(server=args.registry,port=args.port)
+for image in reg.imageList():
+    for tag in reg.tagList(image):
+        print image+":"+tag
